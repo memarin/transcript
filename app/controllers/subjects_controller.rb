936d326f4736c7866eaad083df3a9036690b8a61
MICHAEL MARIN <memarin@me.com>
2015-06-16 06:22:00 +0800

    add presence validation in subject model

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,7 +1,7 @@
 class SubjectsController < ApplicationController
   def index
     if params[:search]
-      @courses = Course.where("course_code LIKE ?", "%#{params[:search]}%")
+      @subject = Subject.where("subject_code LIKE ?", "%#{params[:search]}%")
     end
   end
 end
--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,2 +1,5 @@
 class Subject < ActiveRecord::Base
+  validates :subject_code,
+            :subject_name,
+            :units, presence: true
 end
