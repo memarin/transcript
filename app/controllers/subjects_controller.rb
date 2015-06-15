90f435908ada83dbb2a3d1f166285eb970587e13
MICHAEL MARIN <memarin@me.com>
2015-06-16 07:05:01 +0800

    - Add Subject New view - Add new and create methods in Subjects controller

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -4,4 +4,17 @@ class SubjectsController < ApplicationController
       @subject = Subject.where("subject_code LIKE ?", "%#{params[:search]}%")
     end
   end
+
+  def new
+    @subject = Subject.new()
+  end
+
+  def create
+    @subject = Subject.new()
+  end
+
+  private
+    def subject_params
+      params.require(:subject).permit(:subject_name, :subject_code, :units)
+    end
 end
