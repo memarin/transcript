bd57a7f1f2fd7681f94d62597a99b75e509115b1
memarin <memarin@me.com>
2015-06-17 07:09:18 +0000

    add sign out link in header

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,4 +1,5 @@
 class CoursesController < ApplicationController
+  before_action :authenticate_user!
 
   def show
     @course = Course.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
   root 'home#index'
   resources :courses
   resources :subjects
-  resources :users
 end
